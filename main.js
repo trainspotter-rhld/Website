0708d6a25dcecee010781b4777f91f3c3d381cb0
Omega579 <88238063+Omega579@users.noreply.github.com>
2021-12-03 19:47:56 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+    function Support() {
+    alert('Thanks-')
+    alert('For supporting Me!')
+    console.log(`ssButton_${random} Preesed`)
+}
+
+function Help() {
+    alert('Help. You need to Search ')
+}
+
+function Sign_in() {
+    alert('Sign in...')
+}
+
+const random = Math.random();
+
+console.log(random);
